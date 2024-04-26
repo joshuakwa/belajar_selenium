f16d968864f5017514f52be6027d039adf988d0c
DaffaPintu <daffa@pintu.co.id>
2024-04-26 13:37:11 +0700

    FIX

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,23 +1,31 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Selenium {
 
-    @Test
-    public void helloJayjay(){
-        // inisialisasi browser driver
-        WebDriver driver = WebDriverManager.chromedriver().create();
+    public static WebDriver driver;
 
+    @Test
+    public void helloJayjay() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         // navigasi url
         driver.get("https://jayjay.co");
-
-        // get element attribute
-        String text = driver.findElement(By.className("first-course")).getText();
-
-        // assertion
-        assertEquals("Gebrakan Kursus Online yang Siap Meroketkan Kariermu", text);
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
